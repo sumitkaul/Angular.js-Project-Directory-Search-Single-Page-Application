6162c3a15211b9d082ea1272b945c2e71065eb2f
sumit kaul <sumit.kaul.87@gmail.com>
2014-07-30 18:26:16 -0400

    deep linking set-up

--- js/controllers.js
+++ js/controllers.js
@@ -1,7 +1,7 @@
 //this is the creation of the namespace so that we can protect our variables for nameing conflicts
-var myApp = angular.module('myApp',[]);
+var artistControllers = angular.module('artistControllers',[]);
 
-myApp.controller('MyController', ['$scope','$http',function ($scope,$http){
+artistControllers.controller('ListController', ['$scope','$http',function ($scope,$http){
 
 	$http.get("js/data.json").success(function(data){
 	$scope.artists =data;
