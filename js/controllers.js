82d9de97f7ace06547c7eb711674985ceb715834
sumit kaul <sumit.kaul.87@gmail.com>
2014-07-30 18:08:25 -0400

    solving the issue of name not getting selected by default when page first load so that direction filter can work

--- js/controllers.js
+++ js/controllers.js
@@ -5,7 +5,8 @@ myApp.controller('MyController', ['$scope','$http',function ($scope,$http){
 
 	$http.get("js/data.json").success(function(data){
 	$scope.artists =data;
-	
+	$scope.artists ='name';
+
 	});		 
 }]);
 
