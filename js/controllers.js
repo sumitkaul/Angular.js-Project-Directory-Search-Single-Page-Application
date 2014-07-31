67d73c269b168a09074bd30cd3d99c135f43e940
sumit kaul <sumit.kaul.87@gmail.com>
2014-07-31 11:05:44 -0400

    adding navigation to details page

--- js/controllers.js
+++ js/controllers.js
@@ -17,6 +17,22 @@ artistControllers.controller('DetailsController', ['$scope','$http','$routeParam
 	$scope.artists =data;
 	$scope.whichItem = $routeParams.itemId;
 
+		//Prev item
+		//If the item is not the first item in the array scope then get the previous item else get the last item which is at the end of the array length
+		if($routeParams.itemId > 0){
+			//we are adding the number here because the route param will retuen the string for itemId for which we need to subtract 1 so we need to convert that to int first and then subtract 1.
+			$scope.prevItem= Number($routeParams.itemId) -1 ; 
+		} else{
+			$scope.prevItem= $scope.artists.length -1;
+		} 
+
+		//Next Item
+		if($routeParams.itemId < 0){
+			$scope.nextItem= Number($routeParams.itemId) +1 ; 
+		} else{
+			$scope.nextItem= 0;
+		} 
+
 	});		 
 }]);
 
